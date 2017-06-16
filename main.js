75c55a0b42275e0ac89a8315924a5049390b7072
aaronkeith <aflisik@gmail.com>
2017-06-16 09:27:55 -0700

    add shoe gallery thumbs, gallery style, js inventory array start. #3

---
+++ main.js
@@ -0,0 +1,11 @@
+var shoeInventory [
+  {
+    brand: 'NIKE',
+    description: 'AIR MAX 2017',
+    category: 'RUNNING',
+    price: '$190',
+    styleNumber: '849559-001',
+    color: 'Black Anthracite White',
+    sex: 'MEN'
+  }
+]

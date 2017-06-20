a95e94332fdc748219531c390cf81b20e1036ec9
aaronkeith <aflisik@gmail.com>
2017-06-19 20:56:42 -0700

    renderShoe renderGalleryTitle and renderGallery complete. #3

--- main.js
+++ main.js
@@ -1,148 +1,147 @@
 var shoeInventory = [{
-    brand: 'NIKE',
-    style: 'AIR MAX 2017',
-    category: 'RUNNING SHOES',
-    price: 190,
-    styleNumber: '849559-001',
-    color: 'Black/Anthracite/White',
-    numberColors: 3 + ' ' + 'Colors',
-    sex: 'MEN\'s',
-    shoeImage: 'images/RUN/RUN NIKE AIR MAX 2017 MEN\'S RUNNING SHOE $190 Style: 849559-001 Black Anthracite White.jpg'
-  },
-  {
-    brand: 'NIKE',
-    style: 'AIR MAX SEQUENT 2',
-    category: 'RUNNING SHOES',
-    price: 120,
-    styleNumber: '852461-001',
-    color: 'Black/Dark Grey/Wolf Grey/Metallic/Hematite',
-    numberColors: 2 + ' ' + 'Colors',
-    sex: 'MEN\'s',
-    shoeImage: 'images/RUN/RUN NIKE AIR MAX SEQUENT 2 MEN\'S RUNNING SHOE $100 Style: 852461-001 Black Dark-Grey Wolf-Grey Metallic Hematite.jpg'
-  },
-  {
-    brand: 'NIKE',
-    style: 'AIR ZOOM ELITE 8',
-    category: 'RUNNING SHOES',
-    price: 115,
-    styleNumber: '748588-001',
-    color: 'Black/Wolf Grey/Dark Grey/White',
-    numberColors: 2 + ' ' + 'Colors',
-    sex: 'MEN\'s',
-    shoeImage: 'images/RUN/RUN NIKE AIR ZOOM ELITE 8 MEN\'S RUNNING SHOE $115 Style: 748588-001 Black Wolf-Grey Dark-Grey White.jpg'
-  },
-  {
-    brand: 'NIKE',
-    style: 'AIR ZOOM PEGASUS 34',
-    category: 'RUNNING SHOES',
-    price: 110,
-    styleNumber: '880555-403',
-    color: 'Blue-Fox/Crimson/White/Black',
-    numberColors: 3 + ' ' + 'Colors',
-    sex: 'MEN\'s',
-    shoeImage: 'images/RUN/RUN NIKE AIR ZOOM PEGASUS 34 MEN\'S RUNNING SHOE $110 Style: 880555-403 Blue-Fox Crimson White Black.jpg'
-  },
-  {
-    brand: 'NIKE',
-    style: 'AIR ZOOM TERRA KIGER 4',
-    category: 'RUNNING SHOES',
-    price: 110,
-    styleNumber: '880563-001',
-    color: 'Black/Volt/Hyper-Turquoise/White',
-    numberColors: 2 + ' ' + 'Colors',
-    sex: 'MEN\'s',
-    shoeImage: 'images/RUN/RUN NIKE AIR ZOOM TERRA KIGER 4 MEN\'S RUNNING SHOE $125 Style: 880563-001 Black Volt Hyper-Turquoise White.jpg'
-  },
-  {
-    brand: 'NIKE',
-    style: 'SB AIR MAX BRUIN VAPOR',
-    category: 'SKATE SHOES',
-    price: 100,
-    styleNumber: '882097-001',
-    color: 'Black/White/Cool-Grey',
-    numberColors: 3 + ' ' + 'Colors',
-    sex: 'MEN\'s',
-    shoeImage: 'images/SKATE/SKATE NIKE SB AIR MAX BRUIN VAPOR MEN\'S SKATEBOARDING SHOE $100 Style- 882097-001 Black White White Cool-Grey.jpg'
-  },
-  {
-    brand: 'NIKE',
-    style: 'SB BLAZER LOW GT',
-    category: 'SKATE SHOES',
-    price: 75,
-    styleNumber: '704939-018',
-    color: 'Gunsmoke/Gum Light/Orange Blaze/Black Spruce',
-    numberColors: 1 + ' ' + 'Colors',
-    sex: 'MEN\'s',
-    shoeImage: 'images/SKATE/SKATE NIKE SB BLAZER LOW GT MEN\'S SKATEBOARDING SHOE $75 Style- 704939-018 Gunsmoke Gum Light Orange-Blaze Black-Spruce.jpg'
-  },
-  {
-    brand: 'NIKE',
-    style: 'SB DUNK HIGH PRO',
-    category: 'SKATE SHOES',
-    price: 100,
-    styleNumber: '854851-010',
-    color: 'Dark Grey/Black/White/White',
-    numberColors: 3 + ' ' + 'Colors',
-    sex: 'MEN\'s',
-    shoeImage: 'images/SKATE/SKATE NIKE SB DUNK HIGH PRO MEN\'S SKATEBOARDING SHOE $100 Style- 854851-010 Dark-Grey Black White White.jpg'
-  },
-  {
-    brand: 'NIKE',
-    style: 'SB KOSTON 3 HYPERFEEL',
-    category: 'SKATE SHOES',
-    price: 150,
-    styleNumber: '819673-017',
-    color: 'Black/Yellow Strike/Gum/White',
-    numberColors: 2 + ' ' + 'Colors',
-    sex: 'MEN\'s',
-    shoeImage: 'images/SKATE/SKATE NIKE SB KOSTON 3 HYPERFEEL MEN\'S SKATEBOARDING SHOE $150 Style- 819673-017 Black Yellow-StrikeGum White.jpg'
-  },
-  {
-    brand: 'NIKE',
-    style: 'JORDAN SUPER FLY 5 PO',
-    category: 'BALL SHOES',
-    price: 140,
-    styleNumber: '881571-110',
-    color: 'White/White/Black',
-    numberColors: 2 + ' ' + 'Colors',
-    sex: 'MEN\'s',
-    shoeImage: 'images/BALL/BALL JORDAN SUPER FLY 5 PO MEN\'S BASKETBALL SHOE $140 Style: 881571-110 White White Black.jpg'
-  },
-  {
-    brand: 'NIKE',
-    style: 'KOBE A.D. NXT',
-    category: 'BALL SHOES',
-    price: 200,
-    styleNumber: '916832-710',
-    color: 'Volt/White/White',
-    numberColors: 1 + ' ' + 'Colors',
-    sex: 'MEN\'s',
-    shoeImage: 'images/BALL/BALL KOBE A.D. NXT MEN\'S BASKETBALL SHOE $200 Style: 916832-710 Volt White White.jpg'
-  },
-  {
-    brand: 'NIKE',
-    style: 'LEBRON XIV AGIMAT',
-    category: 'BALL SHOES',
-    price: 175,
-    styleNumber: '852402-400',
-    color: 'Coastal Blue/Star Blue/White',
-    numberColors: 2 + ' ' + 'Colors',
-    sex: 'MEN\'s',
-    shoeImage: 'images/BALL/BALL LEBRON XIV AGIMAT MEN\'S BASKETBALL SHOE $175 Style: 852402-400 Coastal-Blue Star-Blue White.jpg'
-  },
-  {
-    brand: 'NIKE',
-    style: 'KYRIE 2 iD',
-    category: 'BALL SHOES',
-    price: 200,
-    styleNumber: '852402-666',
-    color: 'Black/Pure Gold/White',
-    numberColors: 2 + ' ' + 'Colors',
-    sex: 'MEN\'s',
-    shoeImage: 'images/BALL/BALL NIKE KYRIE 2 iD_Black Gold White .jpg'
-  }
+  brand: 'NIKE',
+  style: 'AIR MAX 2017',
+  category: 'RUNNING SHOES',
+  price: 190,
+  styleNumber: '849559-001',
+  color: 'Black/Anthracite/White',
+  numberColors: 3 + ' ' + 'Colors',
+  sex: 'MEN\'s',
+  shoeImage: 'images/RUN/RUN NIKE AIR MAX 2017 MEN\'S RUNNING SHOE $190 Style: 849559-001 Black Anthracite White.jpg'
+},
+{
+  brand: 'NIKE',
+  style: 'AIR MAX SEQUENT 2',
+  category: 'RUNNING SHOES',
+  price: 120,
+  styleNumber: '852461-001',
+  color: 'Black/Dark Grey/Wolf Grey/Metallic/Hematite',
+  numberColors: 2 + ' ' + 'Colors',
+  sex: 'MEN\'s',
+  shoeImage: 'images/RUN/RUN NIKE AIR MAX SEQUENT 2 MEN\'S RUNNING SHOE $100 Style: 852461-001 Black Dark-Grey Wolf-Grey Metallic Hematite.jpg'
+},
+{
+  brand: 'NIKE',
+  style: 'AIR ZOOM ELITE 8',
+  category: 'RUNNING SHOES',
+  price: 115,
+  styleNumber: '748588-001',
+  color: 'Black/Wolf Grey/Dark Grey/White',
+  numberColors: 2 + ' ' + 'Colors',
+  sex: 'MEN\'s',
+  shoeImage: 'images/RUN/RUN NIKE AIR ZOOM ELITE 8 MEN\'S RUNNING SHOE $115 Style: 748588-001 Black Wolf-Grey Dark-Grey White.jpg'
+},
+{
+  brand: 'NIKE',
+  style: 'AIR ZOOM PEGASUS 34',
+  category: 'RUNNING SHOES',
+  price: 110,
+  styleNumber: '880555-403',
+  color: 'Blue-Fox/Crimson/White/Black',
+  numberColors: 3 + ' ' + 'Colors',
+  sex: 'MEN\'s',
+  shoeImage: 'images/RUN/RUN NIKE AIR ZOOM PEGASUS 34 MEN\'S RUNNING SHOE $110 Style: 880555-403 Blue-Fox Crimson White Black.jpg'
+},
+{
+  brand: 'NIKE',
+  style: 'AIR ZOOM TERRA KIGER 4',
+  category: 'RUNNING SHOES',
+  price: 110,
+  styleNumber: '880563-001',
+  color: 'Black/Volt/Hyper-Turquoise/White',
+  numberColors: 2 + ' ' + 'Colors',
+  sex: 'MEN\'s',
+  shoeImage: 'images/RUN/RUN NIKE AIR ZOOM TERRA KIGER 4 MEN\'S RUNNING SHOE $125 Style: 880563-001 Black Volt Hyper-Turquoise White.jpg'
+},
+{
+  brand: 'NIKE',
+  style: 'SB AIR MAX BRUIN VAPOR',
+  category: 'SKATE SHOES',
+  price: 100,
+  styleNumber: '882097-001',
+  color: 'Black/White/Cool-Grey',
+  numberColors: 3 + ' ' + 'Colors',
+  sex: 'MEN\'s',
+  shoeImage: 'images/SKATE/SKATE NIKE SB AIR MAX BRUIN VAPOR MEN\'S SKATEBOARDING SHOE $100 Style- 882097-001 Black White White Cool-Grey.jpg'
+},
+{
+  brand: 'NIKE',
+  style: 'SB BLAZER LOW GT',
+  category: 'SKATE SHOES',
+  price: 75,
+  styleNumber: '704939-018',
+  color: 'Gunsmoke/Gum Light/Orange Blaze/Black Spruce',
+  numberColors: 1 + ' ' + 'Colors',
+  sex: 'MEN\'s',
+  shoeImage: 'images/SKATE/SKATE NIKE SB BLAZER LOW GT MEN\'S SKATEBOARDING SHOE $75 Style- 704939-018 Gunsmoke Gum Light Orange-Blaze Black-Spruce.jpg'
+},
+{
+  brand: 'NIKE',
+  style: 'SB DUNK HIGH PRO',
+  category: 'SKATE SHOES',
+  price: 100,
+  styleNumber: '854851-010',
+  color: 'Dark Grey/Black/White/White',
+  numberColors: 3 + ' ' + 'Colors',
+  sex: 'MEN\'s',
+  shoeImage: 'images/SKATE/SKATE NIKE SB DUNK HIGH PRO MEN\'S SKATEBOARDING SHOE $100 Style- 854851-010 Dark-Grey Black White White.jpg'
+},
+{
+  brand: 'NIKE',
+  style: 'SB KOSTON 3 HYPERFEEL',
+  category: 'SKATE SHOES',
+  price: 150,
+  styleNumber: '819673-017',
+  color: 'Black/Yellow Strike/Gum/White',
+  numberColors: 2 + ' ' + 'Colors',
+  sex: 'MEN\'s',
+  shoeImage: 'images/SKATE/SKATE NIKE SB KOSTON 3 HYPERFEEL MEN\'S SKATEBOARDING SHOE $150 Style- 819673-017 Black Yellow-StrikeGum White.jpg'
+},
+{
+  brand: 'NIKE',
+  style: 'JORDAN SUPER FLY 5 PO',
+  category: 'BALL SHOES',
+  price: 140,
+  styleNumber: '881571-110',
+  color: 'White/White/Black',
+  numberColors: 2 + ' ' + 'Colors',
+  sex: 'MEN\'s',
+  shoeImage: 'images/BALL/BALL JORDAN SUPER FLY 5 PO MEN\'S BASKETBALL SHOE $140 Style: 881571-110 White White Black.jpg'
+},
+{
+  brand: 'NIKE',
+  style: 'KOBE A.D. NXT',
+  category: 'BALL SHOES',
+  price: 200,
+  styleNumber: '916832-710',
+  color: 'Volt/White/White',
+  numberColors: 1 + ' ' + 'Colors',
+  sex: 'MEN\'s',
+  shoeImage: 'images/BALL/BALL KOBE A.D. NXT MEN\'S BASKETBALL SHOE $200 Style: 916832-710 Volt White White.jpg'
+},
+{
+  brand: 'NIKE',
+  style: 'LEBRON XIV AGIMAT',
+  category: 'BALL SHOES',
+  price: 175,
+  styleNumber: '852402-400',
+  color: 'Coastal Blue/Star Blue/White',
+  numberColors: 2 + ' ' + 'Colors',
+  sex: 'MEN\'s',
+  shoeImage: 'images/BALL/BALL LEBRON XIV AGIMAT MEN\'S BASKETBALL SHOE $175 Style: 852402-400 Coastal-Blue Star-Blue White.jpg'
+},
+{
+  brand: 'NIKE',
+  style: 'KYRIE 2 iD',
+  category: 'BALL SHOES',
+  price: 200,
+  styleNumber: '852402-666',
+  color: 'Black/Pure Gold/White',
+  numberColors: 2 + ' ' + 'Colors',
+  sex: 'MEN\'s',
+  shoeImage: 'images/BALL/BALL NIKE KYRIE 2 iD_Black Gold White .jpg'
+}
 ]
-//****click static image to dring up relevant category****
 
 function extractByCategory(category, shoes) {
   var categoryMatch = shoes.filter(function(shoe) {
@@ -151,56 +150,112 @@ function extractByCategory(category, shoes) {
   return categoryMatch
 }
 
+//****click static image to dring up relevant category****
+//RUN CLICK TO RENDER GALLERY
 var clickStaticRun = document.querySelector("#static-run")
-clickStaticRun.addEventListener('click', extractByCategory('RUNNING SHOES', shoeInventory))
+clickStaticRun.addEventListener('click', function() {
+  // Get all the running shoes data objects.
+  var runningShoes = extractByCategory('RUNNING SHOES', shoeInventory)
 
+  // Clear the gallery.
+  var $gallery = document.querySelector('.gallery-main')
+  $gallery.innerHTML = ''
+
+  $gallery.appendChild(renderGallery(runningShoes[0].category, runningShoes.length, runningShoes))
+})
+//RUN tag CLICK TO RENDER GALLERY
+var clickRunT = document.querySelector("#run-carousel-tag")
+clickRunT.addEventListener('click', function() {
+  // Get all the running shoes data objects.
+  var runningShoes = extractByCategory('RUNNING SHOES', shoeInventory)
+
+  // Clear the gallery.
+  var $gallery = document.querySelector('.gallery-main')
+  $gallery.innerHTML = ''
+
+  $gallery.appendChild(renderGallery(runningShoes[0].category, runningShoes.length, runningShoes))
+})
+//SKATE CLICK TO RENDER GALLERY
 var clickStaticSkate = document.querySelector("#static-skate")
-clickStaticRun.addEventListener('click', extractByCategory('SKATE SHOES', shoeInventory))
+clickStaticSkate.addEventListener('click', function() {
+  // Get all the running shoes data objects.
+  var skateShoes = extractByCategory('SKATE SHOES', shoeInventory)
 
+  // Clear the gallery.
+  var $gallery = document.querySelector('.gallery-main')
+  $gallery.innerHTML = ''
+
+  $gallery.appendChild(renderGallery(skateShoes[0].category, skateShoes.length, skateShoes))
+})
+//SKATE tag CLICK TO RENDER GALLERY
+var clickSkateT = document.querySelector("#skate-carousel-tag")
+clickSkateT.addEventListener('click', function() {
+  // Get all the running shoes data objects.
+  var skateShoes = extractByCategory('SKATE SHOES', shoeInventory)
+
+  // Clear the gallery.
+  var $gallery = document.querySelector('.gallery-main')
+  $gallery.innerHTML = ''
+
+  $gallery.appendChild(renderGallery(skateShoes[0].category, skateShoes.length, skateShoes))
+})
+//BALL CLICK TO RENDER GALLERY
 var clickStaticBall = document.querySelector("#static-ball")
-clickStaticRun.addEventListener('click', extractByCategory('BALL SHOES', shoeInventory))
+clickStaticBall.addEventListener('click', function() {
+  // Get all the running shoes data objects.
+  var ballShoes = extractByCategory('BALL SHOES', shoeInventory)
 
-// "static-run".onclick = function() {
-//   extractByCategory('RUNNING SHOES', shoeInventory)
-// }
+  // Clear the gallery.
+  var $gallery = document.querySelector('.gallery-main')
+  $gallery.innerHTML = ''
+
+  $gallery.appendChild(renderGallery(ballShoes[0].category, ballShoes.length, ballShoes))
+})
+//BALL tag CLICK TO RENDER GALLERY
+var clickBallT = document.getElementById("ball-carousel-tag")
+clickBallT.addEventListener('click', function() {
+  // Get all the running shoes data objects.
+  var ballShoes = extractByCategory('BALL SHOES', shoeInventory)
+
+  // Clear the gallery.
+  var $gallery = document.querySelector('.gallery-main')
+  // $gallery.innerHTML = ''
+
+  $gallery.appendChild(renderGallery(ballShoes[0].category, ballShoes.length, ballShoes))
+})
+
+
+//RENDER GALLERY FUNCTION
+function renderGallery(title, count, shoes) {
+  var $galleryContent = document.createElement('div')
+
+  var $title = renderGalleryTitle(title, count)
+
+  var $galleryItems = document.createElement('div')
+  $galleryItems.classList.add('container')
+  $galleryItems.classList.add('row')
+  $galleryItems.setAttribute('id', 'gallery-items')
+
+  for (var i = 0; i < shoes.length; i++) {
+    var sneaker = renderShoe(shoes[i])
+    $galleryItems.appendChild(sneaker)
+  }
+
+  $galleryContent.appendChild($title)
+  $galleryContent.appendChild($galleryItems)
+
+  return $galleryContent
+}
+
+// RENDER GALLERY LOOP
+// for (var i = 0; i < shoeInventory.length; i++) {
+//   var galleryTitle = renderGalleryTitle(shoeInventory[i])
+//   document.querySelector("#gallery-main").appendChild(galleryTitle)
+//  }
 //
-// "static-skate".onclick = function() {
-//   extractByCategory('SKATE SHOES', shoeInventory)
-// }
-//
-// "static-ball".onclick = function() {
-//   extractByCategory('BALL SHOES', shoeInventory)
-// }
 
-//***************count of how many shoes belong to category
-// var runCategory = shoeInventory.reduce(function(n, shoe) {
-//   return n + (shoe.category == 'RUNNING SHOES');
-// }, 0);
-//
-// var skateCategory = shoeInventory.reduce(function(n, shoe) {
-//   return n + (shoe.category == 'SKATE SHOES');
-// }, 0);
-//
-// var ballCategory = shoeInventory.reduce(function(n, shoe) {
-//   return n + (shoe.category == 'BALL SHOES');
-// }, 0);
-
-//*****FIXME LOOP THROUGH ALL SHOES AND PRINTS ALL THE CATEGORIES ONLY WANT TO PRINT A SINGLE CATEGORY***NOT WORKING
-// window.onload = function () {
-for (var i = 0; i < shoeInventory.length; i++) {
-  var galleryTitle = renderGalleryTitle(shoeInventory[i])
-  document.querySelector("#gallery-main").appendChild(galleryTitle)
-  // console.log(galleryTitle)
- }
-//
-
-////////***********FIXME Below Renders ALL Gallery Titles when I only want to show the instance a single time*****************
-
-function renderGalleryTitle(galleryTitle) {
-
-  var countCategory = shoeInventory.reduce(function(n, shoe) {
-    return n + (shoe.category === galleryTitle.category);
-  }, 0);
+//RENDER GALLERY TITLE
+function renderGalleryTitle(title, count) {
 
   var $galleryHeader = document.createElement('div')
   $galleryHeader.classList.add('gallery-title-bar')
@@ -208,11 +263,11 @@ function renderGalleryTitle(galleryTitle) {
 
   var $galleryTitle = document.createElement('span')
   $galleryTitle.classList.add('gallery-title')
-  $galleryTitle.textContent = galleryTitle.category
+  $galleryTitle.textContent = title
 
   var $galleryQty = document.createElement('span')
   $galleryQty.classList.add('gallery-shoe-qty')
-  $galleryQty.textContent = '(' + ' ' + countCategory + ' ' + ')'
+  $galleryQty.textContent = '(' + count + ')'
 
   $galleryHeader.appendChild($galleryTitle)
   $galleryHeader.appendChild($galleryQty)
@@ -220,14 +275,12 @@ function renderGalleryTitle(galleryTitle) {
   return $galleryHeader
 }
 
-////////***********FIXME Below Renders ALL shoes and append them to html*****************
+//RENDERS A SINGLE SHOE...IF THE FOR LOOP IS NOT USED.
 
-// window.onload = function () {
-for (var i = 0; i < shoeInventory.length; i++) {
-  var sneaker = renderShoe(shoeInventory[i])
-  document.querySelector("#gallery-items").appendChild(sneaker)
-  // console.log(sneaker)
-}
+// for (var i = 0; i < shoeInventory.length; i++) {
+//   var sneaker = renderShoe(shoeInventory[i])
+//   document.querySelector("#gallery-items").appendChild(sneaker)
+//   // console.log(sneaker)
 // }
 
 function renderShoe(shoeData) {
